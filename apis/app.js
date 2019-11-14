900a0d16b4c3276b3573e724733e7117813f72d6
WILLIAMS RUANO <wgruano@correo.url.edu.gt>
2019-11-13 20:09:37 -0600

    verbos funcionando en docker

--- apis/app.js
+++ apis/app.js
@@ -8,8 +8,12 @@ var mongoose = require("mongoose");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var peliculasRouter = require('./routes/peliculas');
+var cors = require('cors');
 
 var app = express();
+
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/api/v1/peliculas', peliculasRouter);
